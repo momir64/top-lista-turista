8c9c93c54e4086dde26bbd761fb89b02f11ed9ba
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-04-24 05:47:43 +0200

    Update

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -7,7 +7,36 @@
           id="content_holder"
           class="align-center text-center px-xs-3 px-sm-5 px-md-11"
         >
-          Agencije
+          <v-btn
+            class="mb-9"
+            height="50px"
+            width="270px"
+            variant="outlined"
+            prependIcon="mdi-plus"
+          >
+            dodaj novu agenciju
+          </v-btn>
+
+          <v-list
+            class="px-6 py-8 border-opacity-100"
+            rounded="xl"
+            border="sm"
+            bg-color="#f8f0e0"
+            lines="one"
+          >
+            <v-list-item v-for="(agencija, i) in agencije" :key="agencija.id">
+              <template v-slot:title>
+                <div class="float-left pe-10">
+                  {{ (i + 1) * (i + 1) * (i + 1) }}
+                </div>
+                <span class="float-left">{{ agencija.naziv }}</span>
+              </template>
+              <template v-slot:append>
+                <v-btn icon="mdi-pencil" variant="plain"></v-btn>
+                <v-btn icon="mdi-delete" variant="plain"></v-btn>
+              </template>
+            </v-list-item>
+          </v-list>
         </v-card>
       </v-container>
     </v-main>
@@ -36,24 +65,25 @@
   min-height: calc(100vh - 80px - 90px);
   background-color: #f8f0e0;
   padding-top: 45px;
+  padding-bottom: 64px;
   border-radius: 0;
 }
 #content_holder_container {
-  padding-right: 10vw;
-  padding-left: 10vw;
+  padding-right: 20vw;
+  padding-left: 20vw;
   padding-top: 16px;
   padding-bottom: 0;
   max-width: 100vw;
 }
-@media screen and (max-width: 1600px) {
+@media screen and (max-width: 1280px) {
   #content_holder_container {
-    padding-left: 8vw !important;
-    padding-right: 8vw !important;
+    padding-left: 15vw !important;
+    padding-right: 15vw !important;
   }
 }
 @media screen and (max-width: 599.9px) {
   #content_holder {
-    padding-top: 10px;
+    padding-top: 20px;
   }
 }
 @media screen and (max-width: 850px) {
@@ -75,6 +105,7 @@ export default {
   },
   data: () => ({
     url: "https://top-lista-turista-default-rtdb.europe-west1.firebasedatabase.app/",
+    agencije: [],
     fab: null,
   }),
   methods: {
@@ -83,10 +114,39 @@ export default {
     },
     check() {
       this.fab = window.pageYOffset > 60;
-    },    
+    },
+    async load_agencije() {
+      let code, message;
+      try {
+        let response = await fetch(this.url + "/agencije.json");
+        code = response.status;
+        message = response.statusText;
+        if (!response.ok) throw new Error();
+
+        const data = await response.json();
+        const results = [];
+
+        for (const id in data) {
+          results.push({
+            id: id,
+            logo: data[id]["logo"],
+            naziv: data[id]["naziv"],
+            adresa: data[id]["adresa"],
+            email: data[id]["email"],
+            telefon: data[id]["brojTelefona"],
+          });
+        }
+        this.agencije = results;
+      } catch (e) {
+        console.log(e);
+        message = `Firebase: ${code}\u00A0${message}`;
+        code = "Ooops";
+        this.$router.push({ path: "/error", state: { code, message } });
+      }
+    },
   },
   beforeMount() {
-    // this.load_destinacije();
+    this.load_agencije();
   },
 };
 </script>
